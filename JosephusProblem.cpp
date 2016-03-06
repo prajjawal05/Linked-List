2a98538685c0950d56155af2badc7f8e912d52e4
Prajjawal Agarwal <prajjawalagarwal@gmail.com>
2016-03-06 21:57:17 +0530

    Update JosephusProblem.cpp

--- JosephusProblem.cpp
+++ JosephusProblem.cpp
@@ -3,40 +3,73 @@
 struct node
 {
 	int data;
-	struct node* next;
+	struct node *next;
 };
-void Print(struct node *head)
+void* insert(struct node *head,struct node *tail,int n);
+void jos(struct node *head,int m);
+void print(struct node *head);
+int main()
 {
-	struct node*temp;
-	temp=head;
-	while(temp!=NULL)
+	printf("Enter number of elements to be inserted\n");
+	int n,m;
+	scanf("%d",&n);
+	struct node *head=NULL,*tail=NULL;
+	//printf("1\n");
+	head=insert(head,tail,n);
+	print(head);
+	printf("ENTER the Joseph constant\n");	
+	scanf("%d",&m);
+        jos(head,m);
+	return 0;
+}
+void* insert(struct node *head,struct node *tail,int n)
+{
+	int i;
+	struct node *temp;
+	temp=(struct node*)malloc(sizeof(struct node**));
+	int y;
+	scanf("%d",&y);
+	temp->data=y;
+	//printf("1\n");
+	temp->next=NULL;
+	//printf("1\n");
+	head=temp;
+	tail=temp;
+	//printf("%d\n",temp->data);
+	for(i=2;i<=n;i++)
+	{
+		temp=(struct node*)malloc(sizeof(struct node*));
+		int x;
+		scanf("%d",&x);
+		temp->data=x;
+		tail->next=temp;
+		tail=temp;
+		tail->next=NULL;
+		//printf("%d\n",tail->data);
+	}
+	tail->next=head;
+	return head;
+}
+void print(struct node *head)
+{
+	int i;
+	struct node*temp=head;
+	for(i=0;i<13;i++)
 	{
 		printf("%d\n",temp->data);
 		temp=temp->next;
 	}
 }
-int main()
+void jos(struct node *head, int m)
 {
-	struct node*head,*temp,*tail;
-	head=NULL,temp=NULL,tail=NULL;
 	int i;
-	for(i=1;i<=3;i++)
+	while(head->next!=head)
 	{
-		temp=(struct node*)malloc(sizeof(struct node));
-		temp->data=i;
-		if(head==NULL)
-		{
-			head=temp;
-			tail=temp;
-			tail->next=NULL;
-		}
-		else
-		{
-			tail->next=temp;
-			tail=temp;
-			tail->next=NULL;	
-		}	
+		for(i=1;i<m-1;i++)
+			head=head->next;
+		head->next=head->next->next;
+		head=head->next;
 	}
-	Print(head);
-	return 0;	
-}
+	printf("%d\n",head->data);
+}	
+		
